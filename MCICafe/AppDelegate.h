4c6084ec3ccf217a3d019e07329e5a44c77d555f
Abdiel Avila Arias <abdiel28@Abdiels-MacBook-Air.local>
2015-02-07 18:04:37 -0500

    Initial Commit

---
+++ MCICafe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCICafe
+//
+//  Created by Abdiel Avila Arias on 2/7/15.
+//  Copyright (c) 2015 Abdiel Avila Arias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
